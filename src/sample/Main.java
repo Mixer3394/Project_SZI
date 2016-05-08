e3654e4be1bb62cb16f723c20ddd8afd41cc15cf
Uncpy <jakub.janicki@icloud.com>
2016-05-08 21:40:44 +0200

    Improved oil

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -115,7 +115,7 @@ public class Main extends Application {
             {12, 10},
             {12, 13}, {11, 13}, {10, 13}, {9, 13}, {8, 13}, {7, 13}, {6, 13}, {5, 13}, {4, 13},
             {3, 13}, {3, 14}, {4, 14}, {5, 14}, {6, 14}, {7, 14}, {8, 14}, {9, 14}, {10, 14},
-            {11, 14}, {12, 14},
+            {11, 14}, {12, 14}, {15, 15}, {15, 15}, {15, 15}, {15, 15}, {15, 15}, {15, 15}, {15, 15}, {15, 15}, {15, 15}, {15, 15}
 
 
     };
@@ -134,10 +134,11 @@ public class Main extends Application {
             {0, 2}, {0, 3}, {0, 4}, {0, 5}, {0, 6}, {0, 7}, {0, 8}, {0, 9}, {0, 10}, {0, 11}, {0, 12},
             {0, 13}, {0, 14}, {0, 15}, {1, 2}, {1, 3}, {1, 4}, {1, 5}, {1, 6}, {1, 7}, {1, 8}, {1, 9}, {1, 10}, {1, 11}, {1, 12},
             {1, 13}, {1, 14}, {1, 15},{2, 0}, {2, 1}, {2, 2}, {2, 3}, {2, 4}, {2, 5}, {2, 6}, {2, 7}, {2, 8}, {2, 9}, {2, 10}, {2, 11}, {2, 12},
-            {2, 13}, {2, 14}, {2, 15}, {3, 0}, {3, 1}, {3, 2}, {3, 3}, {3, 4}, {3, 5}, {3, 6}, {3, 7}, {3, 8}, {3, 9}, {3, 10}, {3, 11}, {3, 12},
-            {3, 13}, {3, 14}, {3, 15}, {14, 0}, {14, 1}, {14, 2}, {14, 3}, {14, 4}, {14, 5}, {14, 6}, {14, 7}, {14, 8}, {14, 9}, {14, 10}, {14, 11}, {14, 12},
+            {2, 13}, {2, 14}, {2, 15}, {14, 0}, {14, 1}, {14, 2}, {14, 3}, {14, 4}, {14, 5}, {14, 6}, {14, 7}, {14, 8}, {14, 9}, {14, 10}, {14, 11}, {14, 12},
             {14, 13}, {14, 14}, {14, 15}, {15, 0}, {15, 1}, {15, 2}, {15, 3}, {15, 4}, {15, 5}, {15, 6}, {15, 7}, {15, 8}, {15, 9}, {15, 10}, {15, 11}, {15, 12},
-            {15, 13}, {15, 14}, {15, 15}, {15, 15}, {15, 15}, {15, 15}, {15, 15}, {15, 15}, {15, 15}, {15, 15}, {15, 15}, {15, 15}, {15, 15}
+            {15, 13}, {15, 14}, {15, 15},
+
+
     };
     public int iterator = 0;
 
@@ -172,7 +173,7 @@ public class Main extends Application {
 
         // ZAMIANA NA WSPÓŁRZĘDNE
         for(int i = 0; i < oilArray.length; i++) {
-            graphicsContext.drawImage(oilSlick, oilsCoordinates[i][0] + 21, oilsCoordinates[i][1] - 15);
+            graphicsContext.drawImage(oilSlick, oilsCoordinates[i][0] + 5, oilsCoordinates[i][1] + 25);
         }
 
 
@@ -279,10 +280,10 @@ public class Main extends Application {
 
         for(int i = 0; i < oilArray.length; i ++) {
             for(int j = 0; j < algorithmAvailablePoints.size(); j++) {
-                if((algorithmAvailablePoints.get(j).getX() == astarBlockedPoints[70 + i][0]) &&
-                        algorithmAvailablePoints.get(j).getY() == astarBlockedPoints[70 + i][1]) {
+                if((algorithmAvailablePoints.get(j).getX() == astarBlockedPoints[80 + i][0]) &&
+                        algorithmAvailablePoints.get(j).getY() == astarBlockedPoints[80 + i][1]) {
 
-                    System.out.print(j + "Dla: X: " + astarBlockedPoints[70 + i][0] + " Y: " + astarBlockedPoints[70 + i][1] + "\n");
+                    System.out.print(j + "Dla: X: " + astarBlockedPoints[80 + i][0] + " Y: " + astarBlockedPoints[80 + i][1] + "\n");
                     oilsToDraw[i] = j;
                 }
             }
@@ -357,13 +358,13 @@ public class Main extends Application {
 //        }
         for(int i = 0; i < 10; i++) {
 
-            int randOil = oilRandom.nextInt(pointsForOil.length) - 1;
+            int randOil = oilRandom.nextInt(pointsForOil.length);
 //            while(!contains(oilArray, randOil)) {
 //
 //                randOil = oilRandom.nextInt(pointsForOil.length) - 1;
 //            }
             oilArray[i] = randOil;
-            astarBlockedPoints[70 + i] = pointsForOil[randOil];
+            astarBlockedPoints[80 + i] = pointsForOil[randOil];
 
         }
 
